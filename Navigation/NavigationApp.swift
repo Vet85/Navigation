9f8de414e6b462faac8662282cd3e39f349b1c47
Vitaliy Novichenko <novichenkovitaliy@gmail.com>
2025-03-30 23:56:50 +0300

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Vitaliy Novichenko on 30.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
